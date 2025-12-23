1817a90f1d1b5c4d005168c3d4bcff687e12f8ed
nevyana <nevyana@composity.com>
2025-12-23 12:48:27 +0200

    fix

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,10 +8,10 @@ const Hero = () => {
           {/* Left Content */}
           <div className="col-lg-5 order-2 order-lg-1 d-flex flex-column justify-content-center">
             <h1 data-aos="fade-up">
-              Елате гладни,<br />тръгнете щастливи!
+              Ресторант-пицария Централ Добрич!
             </h1>
             <p data-aos="fade-up" data-aos-delay="100">
-              Предупреждение: храната ни води до пристрастяване!
+            Храна, приготвена с внимание и поднесена с отношение.
             </p>
             <div className="d-flex" data-aos="fade-up" data-aos-delay="200">
               <a href="#book-a-table" className="btn-get-started">
@@ -37,14 +37,17 @@ const Hero = () => {
             className="col-lg-5 order-1 order-lg-2 hero-img"
             data-aos="zoom-out"
           >
-            <Image
-              src="/images/salmon_dish_transparent.png"
-              alt="Вкусни ястия от ресторант-пицария Централ Добрич"
-              width={500}
-              height={500}
-              className="img-fluid animated"
-              priority
-            />
+            <div style={{ width: '100%', maxWidth: '500px', aspectRatio: '1', margin: '0 auto', borderRadius: '50%', overflow: 'hidden' }}>
+              <Image
+                src="/images/gallery/gallery-winter.jpg"
+                alt="Вкусни ястия от ресторант-пицария Централ Добрич"
+                width={500}
+                height={500}
+                className="img-fluid animated"
+                style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+                priority
+              />
+            </div>
           </div>
         </div>
       </div>
